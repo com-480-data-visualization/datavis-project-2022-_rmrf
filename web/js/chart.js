05f7ec8466328a3b8657660cc7f98ad42399dc5e
Zewei Xu <zewei.xu@epfl.ch>
2022-05-18 13:26:57 +0200

    add new function

--- web/js/chart.js
+++ web/js/chart.js
@@ -1,80 +1,113 @@
 var margin = {top: 30, right: 30, bottom: 70, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+    width,
+    height;
 var df;
-// Parse the Data
+var x,y,xAxis,yAxis;
+var species_code='bbwduc';
+var region_type='country';
+var season_name='breeding';
+var country_code=undefined;
+var key='abundance_mean';
 d3.csv("http://localhost:8000/PycharmProjects/datavis-project-2022-_rmrf/data/data.csv", function(data) {
-    df=data.filter(d => d.species_code=='grerhe1')
-    charPlot(df,key='total_pop_percent')
-
+    df=data;
+    init();
 })
-
-var x,y;
-
-function charPlot(data,key) {
-
-    data.sort(function(b, a) {
-        return a[key] - b[key];
-    });
+function init(){
+    width = 800 - margin.left - margin.right;
+    height = 500 - margin.top - margin.bottom;
+    var svg = d3.select("#bar_plot")
+        .append("svg")
+        .attr('id','chartSvg')
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr('id','bar')
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
     x = d3.scaleBand()
         .range([ 0, width ])
-        .domain(data.map(function(d) { return d.country; }))
         .padding(0.2);
-    svg.append("g")
+    xAxis = svg.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-45)")
-        .style("text-anchor", "end");
 
-// Add Y axis
-     y = d3.scaleLinear()
-        .domain([0, 1])
-        .range([ height, 0]);
-    svg.append("g")
-        .call(d3.axisLeft(y));
+    y = d3.scaleLinear()
+        .range([height, 0]);
+    yAxis = svg.append("g")
+        .attr("class", "myYaxis")
 
-// Bars
-    svg.selectAll("mybar")
-        .data(data)
-        .enter()
-        .append("rect")
-        .attr("x", function(d) { return x(d.country); })
-        .attr("y", function(d) { return y(d[key]); })
-        .attr("width", x.bandwidth())
-        .attr("height", function(d) { return height - y(d[key]); })
-        .attr("fill", "#69b3a2")
+    update();
 
-    svg.selectAll("rect")
-        .transition()
-        .duration(800)
-        .attr("y", function(d) { return y(d[key]); })
-        .attr("height", function(d) { return height - y(d[key]); })
-        .delay(function(d,i){console.log(i) ; return(i*100)})
 }
 
-function update(data,key) {
+function filter_data(){
+    if( region_type == 'country'){
+        return (df||[]).filter(d=>d.species_code==species_code && d.region_type==region_type && d.season_name==season_name);
+    }
+    else {
+        return (df||[]).filter(d=>d.species_code==species_code && d.region_type==region_type && d.season_name==season_name && d.country_code==country_code);
+    }
+}
 
-    var u = svg.selectAll("rect")
+
+function update() {
+    var data=filter_data(country_code);
+    x.domain(data.map(function (d){return d['region_name']}))
+    xAxis.call(d3.axisBottom(x)).attr('id','xaxis');
+    y.domain( [0, d3.max(data, function(d){ return +d[key]; })] );
+    yAxis.transition().duration(1000).call(d3.axisLeft(y)).attr('id','yaxis');
+    var u = d3.select("#bar_plot svg #bar").selectAll("rect")
         .data(data)
     u.enter()
         .append("rect")
         .merge(u)
         .transition()
         .duration(1000)
-        .attr("x", function(d) { return x(d.country); })
+        .attr("x", function(d) { return x(d['region_name']); })
         .attr("y", function(d) { return y(d[key]); })
         .attr("width", x.bandwidth())
         .attr("height", function(d) { return height - y(d[key]); })
-        .attr("fill", "#69b3a2")
+        .attr("fill", "#69b3a2");
+    u.exit()
+        .remove();
+    setEvent();
+}
+
+function setEvent(){
+    d3.select("#bar_plot svg").selectAll("rect")
+        .on('click',function (d){
+            d3.event.stopPropagation();
+            console.log(d)
+            if(region_type=='country'){
+                console.log('inside');
+                region_type='state';
+                country_code=d.country_code;
+                update();
+            }
+        });
+    d3.select("#bar_plot svg").on('click',function (d){
+        if(region_type=='state'){
+            region_type='country';
+            country_code=undefined;
+            update();
+        }
+    }).call(zoom);
+
+
+}
+
+function zoom(svg) {
+    const extent = [[0, margin.top], [width + margin.left, height + margin.bottom]];;
+
+    svg.call(d3.zoom()
+        .scaleExtent([1, 4])
+        .translateExtent(extent)
+        .on("zoom", zoomed));
+
+    function zoomed() {
+        x.range([0, width].map(d =>d3.event.transform.applyX(d)));
+        svg.selectAll("rect").attr("x", d => x(d['region_name'])).attr("width", x.bandwidth());
+        xAxis.call(d3.axisBottom(x)).attr('id','xaxis');
+
+    }
 }
--- web/js/map.js
+++ web/js/map.js
@@ -1,44 +1,161 @@
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+var width, height, projection, path, zoom,data, currentAttribute = 0, playing = false;
+var legendQuantize,colorScale;
+var topo;
+var species_code='bbwduc';
+var season_name='year_round';
+var region_type='country';
+var data_type='abundance_mean';
+var seasons=[];
+var tooltip;
 
-// Map and projection
-var path = d3.geoPath();
-var projection = d3.geoMercator()
-    .scale(70)
-    .center([0,20])
-    .translate([width / 2, height / 2]);
+function zoomed() {
+    d3.selectAll('#mapPath')
+        .attr('transform', d3.event.transform);
+}
 
-// Data and color scale
-var data = d3.map();
-var colorScale = d3.scaleThreshold()
-    .domain([0.01, 0.03, 0.1, 0.3, 1])
-    .range(d3.schemeBlues[7]);
+function init() {
+    setMap();
+    //bind();
+    animateMap()
+}
 
-// Load external data and boot
-d3.queue()
-    .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
-    .defer(d3.csv, "http://localhost:8000/PycharmProjects/datavis-project-2022-_rmrf/data/data.csv", function(d) {
-        if (d.species_code=='grerhe1'){
-            data.set(d.country_code, +d.abundance_mean);
-        }
-    }).await(ready);
+function bind(){
+    d3.selectAll(".m")
+        .on("click", function() {
+            data_type = this.getAttribute("value");
+            drawMap();
+        });
+}
 
-function ready(error, topo) {
-    console.log(topo);
-    // Draw the map
+function setMap() {
+
+    width = 800, height = 500;  // map width and height, matches
+
+    data=d3.map();
+    projection = d3.geoMercator()
+        .translate([width / 2, height / 2])
+        .scale((width - 1) / 2 / Math.PI);
+    path = d3.geoPath().projection(projection);
+
+    colorScale = d3.scaleThreshold()
+        .domain([0.01, 0.03,0.06,0.1,0.3, 0.6, 1])
+        .range(d3.schemeBlues[7]);
+
+    legendQuantize = d3.legendColor()
+        .title("Value")
+        .classPrefix("colorLegend_")
+        .labelFormat(d3.format(".2f"))
+        .shape("rect")
+        .shapePadding(10)
+        .scale(colorScale);
+
+    var svg = d3.select("#map_plot")
+        .append('svg')
+        .attr('id','mapSvg')
+        .attr('width', width)
+        .attr('height', height);
+    svg.append('g').attr('id','map');
+
+    zoom = d3.zoom()
+        .scaleExtent([1, 8])
+        .translateExtent([[0, 0], [width, height]])
+        .on('zoom', zoomed);
+
+    svg.call(zoom);
+    console.log('here')
     svg.append("g")
+        .attr("class", "legendQuantize")
+        .attr("transform", "translate(680,20)");
+    svg.select(".legendQuantize")
+        .call(legendQuantize);
+
+    loadData();  // let's load our data next
+
+}
+
+function loadData() {
+    d3.queue()
+        .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
+        .defer(d3.csv, "http://localhost:8000/PycharmProjects/datavis-project-2022-_rmrf/data/data.csv", function (d){
+            processData(d);
+        })
+        .await(saveData);
+}
+
+function saveData(error,d){
+    topo=d;
+    drawMap()
+}
+function processData(d) {
+    if(d.region_type==region_type){
+        data.set(d.country_code+'_'+d.species_code+'_'+d.season_name+'_abundance_mean', +d.abundance_mean);
+        data.set(d.country_code+'_'+d.species_code+'_'+d.season_name+'_total_pop_percent', +d.total_pop_percent);
+        data.set(d.country_code+'_'+d.species_code+'_'+d.season_name+'_range_percent_occupied', +d.range_percent_occupied);
+        data.set(d.country_code+'_'+d.species_code+'_'+d.season_name+'_range_total_percent', +d.range_total_percent);
+        }
+    if(!seasons.includes(d.season_name)) {
+        seasons.push(d.season_name)
+    }
+
+}
+
+
+function drawMap() {
+    d3.select('#mapSvg')
+        .select("#map")
         .selectAll("path")
         .data(topo.features)
         .enter()
         .append("path")
-        // draw each country
+        .attr('id','mapPath')
         .attr("d", d3.geoPath()
             .projection(projection)
-        )
-        // set the color of each country
-        .attr("fill", function (d) {
-            d.total = data.get(d.id) || 0;
+        ).attr("fill", function (d) {
+            d.total = data.get(d.id + "_" + species_code + '_' + season_name + '_' + data_type) || 0;
             return colorScale(d.total);
+        }).on("mouseover", function (d) {
+            d3.select(this)
+                .attr("fill", "orange");
+        }).on("mouseout", function (d) {
+            d3.select(this)
+                .attr("fill", colorScale(d.total));
+        });
+
+}
+
+function updateMap(){
+    d3.select('#mapSvg').selectAll("#mapPath")
+        .transition()  //select all the countries and prepare for a transition to new values
+        .duration(500)
+        .attr("fill", function (d) {
+            d.total = data.get(d.id + "_" + species_code + '_' + season_name + '_' + data_type) || 0;
+            return colorScale(d.total);
+    })
+}
+
+function animateMap() {
+    var timer;  // create timer object
+    d3.select('#play')
+        .on('click', function() {
+            if(playing == false) {  // if the map is currently playing
+                timer = setInterval(function(){   // set a JS interval
+                    if(currentAttribute < seasons.length-1) {
+                        currentAttribute +=1;  // increment the current attribute counter
+                    } else {
+                        currentAttribute = 0;  // or reset it to zero
+                    }
+                    season_name=seasons[currentAttribute]
+                    updateMap();
+                }, 1000);
+
+                d3.select(this).html('stop');  // change the button label to stop
+                playing = true;   // change the status of the animation
+            } else {    // else if is currently playing
+                clearInterval(timer);   // stop the animation by clearing the interval
+                d3.select(this).html('play');   // change the button label to play
+                playing = false;   // change the status again
+            }
         });
 }
+
+window.onload=init();
